0520ecdfe4661b24329374e509536d33c6a8fad6
Julia N <julia@8thsq.co>
2017-07-30 01:30:37 -0400

    Add AppState

--- src/Stores/AppState.js
+++ src/Stores/AppState.js
@@ -1,7 +1,23 @@
-import { observable, computed } from 'mobx'
+import { observable, computed, action} from 'mobx'
+import Sugar from 'sugar'
 
-class AppState {
+const AppState = observable({
+  activeScenes: [],
+  presentPlayers: [],
+  
+  get totalPresent() {
+    return this.presentPlayers.length
+  },
+  handleRemove: action (function(sceneList) {
+    let scenes = Sugar.Array.create(this.activeScenes)
+    this.activeScenes = sceneList.map((item) => Sugar.Array.remove(scenes, item))
+  }),
 
-}
+  handleAdd: action (function(sceneList) {
+    const scenes = this.activeScenes
+    sceneList.map((item) => scenes.push(item))
+    this.activeScenes = Sugar.Array.unique(scenes)
+  })
+})
 
 export default AppState
